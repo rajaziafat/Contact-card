290c74d8699a837d8be4a0c80071f50b6c666af3
Ali Aalam <aliaalam.me@gmail.com>
2024-03-11 21:52:50 +0500

    a

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -7,7 +7,7 @@ function Card({ onEditClick }) {
 
             <div
 
-                className="block max-w-[320px] md:max-w-[600px]  md:max-h-[340px] py-4 backdrop-blur-lg bg-white bg-opacity-10 rounded-xl border border-white border-opacity-20"
+                className="block max-w-[340px] md:max-w-[600px]  md:max-h-[340px] py-4 backdrop-blur-lg bg-white bg-opacity-10 rounded-xl border border-white border-opacity-20"
             >
 
 
